201bcd4f8e178652c4ad078392e96eda8bb3ff43
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-16 17:42:28 -0700

    adds guess limiting to five

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,42 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-# class WebGuesser
-# end
-NUMBER = rand(100)
+class WebGuesser
+  attr_accessor :counter,
+                :number
+  def initialize
+    @number = rand(100)
+    @counter = 6
+  end
 
-def guiding_message(guess)
-  return "Way too high!"  if (guess.to_i - NUMBER) > 5
-  return "Way too low!"   if (NUMBER - guess.to_i) > 5
-  return "Too high!"      if guess.to_i > NUMBER
-  "Too low!"              if guess.to_i < NUMBER
+  def guiding_message(guess)
+    return "Way too high!"  if (guess.to_i - @number) > 5
+    return "Way too low!"   if (@number - guess.to_i) > 5
+    return "Too high!"      if guess.to_i > @number
+    "Too low!"              if guess.to_i < @number
+  end
+
+  def win_message
+    "You got it right!\nThe SECRET NUMBER is #{@number}"
+  end
+
+  def check_guess(guess)
+    return guiding_message(guess) if guess.to_i != @number
+    win_message                   if guess.to_i == @number
+  end
+
+  def new_game
+    @counter = 6
+    @number = rand(100)
+    "You did not guess the secret number in 5 guesses, so a new number has been set. Begin guessing!"
+  end
+
+  def game_moderator(guess, counter)
+    return check_guess(guess) if counter != 0
+    new_game if counter == 0
+  end
 end
 
-def win_message
-  "You got it right!\nThe SECRET NUMBER is #{NUMBER}"
-end
-
-def check_guess(guess)
-  return guiding_message(guess) if guess.to_i != NUMBER
-  win_message                   if guess.to_i == NUMBER
-end
-
-# def new_game
-#   counter = 6
-#   "You did not guess the secret number in 5 guesses, so a new number has been set. Begin guessing!"
-# end
-
-def game_moderator(guess, counter)
-  return check_guess(guess) if counter != 0
-  # new_game if counter == 0
-end
-
-counter = 6
-
+webguesser = WebGuesser.new
 
 get '/' do
-  counter -= 1
-  message = game_moderator(params['guess'], counter)
-  message = check_guess(params['guess'])
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  webguesser.counter -= 1
+  message = webguesser.game_moderator(params['guess'], webguesser.counter)
+  erb :index, :locals => {:number => webguesser.number, :message => message}
 end
