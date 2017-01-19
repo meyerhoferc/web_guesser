ce9866d7edd450aaea1b1c8059bf51b6a5d8be54
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-19 10:02:03 -0700

    adds background colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,13 +7,14 @@ class WebGuesser
   def initialize
     @number = rand(100)
     @counter = 6
+    @colors = ["backround: green", "background: red", "background: yellow", "background: blue", "background: orange"]
   end
 
   def guiding_message(guess)
-    return "Way too high!"  if (guess.to_i - @number) > 5
-    return "Way too low!"   if (@number - guess.to_i) > 5
-    return "Too high!"      if guess.to_i > @number
-    "Too low!"              if guess.to_i < @number
+    return [@colors[1], "Way too high!"]  if (guess.to_i - @number) > 5
+    return [@colors[2], "Way too low!"]   if (@number - guess.to_i) > 5
+    return [@colors[3], "Too high!"]      if guess.to_i > @number
+    [@colors[4], "Too low!"]              if guess.to_i < @number
   end
 
   def win_message
@@ -22,7 +23,7 @@ class WebGuesser
 
   def check_guess(guess)
     return guiding_message(guess) if guess.to_i != @number
-    win_message                   if guess.to_i == @number
+    [@colors[0], win_message]     if guess.to_i == @number
   end
 
   def new_game
@@ -41,7 +42,6 @@ web_guesser = WebGuesser.new
 
 get '/' do
   web_guesser.counter -= 1
-  message = web_guesser.game_moderator(params['guess'], web_guesser.counter)
-  # have it take in a hex color here to be used in CSS <style> bitness
-  erb :index, :locals => {:number => web_guesser.number, :message => message}
+  color, message = web_guesser.game_moderator(params['guess'], web_guesser.counter)
+  erb :index, :locals => {:number => web_guesser.number, :message => message, :color => color }
 end
